4a6874473a30254d9124f751e834fd1457f22be4
Nugine <nugine@foxmail.com>
2019-05-25 15:27:40 +0800

    implement

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,70 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+pub struct MirageTank {
+    inner: mirage_tank::MirageTank,
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, mirage-tank-wasm-binding!");
+impl MirageTank {
+    #[wasm_bindgen(constructor)]
+    pub fn from_raw(wbuf: &[u8], bbuf: &[u8]) -> Result<MirageTank, JsValue> {
+        let inner = mirage_tank::MirageTank::from_raw(wbuf, bbuf)
+            .map_err::<JsValue, _>(|_| "fail to load image".into())?;
+        Ok(Self { inner })
+    }
+}
+
+#[inline]
+pub(crate) fn is_param(p: f32) -> bool {
+    p.is_normal() && p >= 0.0 && p <= 1.0
+}
+
+#[wasm_bindgen]
+impl MirageTank {
+    pub fn grey_output(
+        &self,
+        width: u32,
+        height: u32,
+        checkerboarded: bool,
+        wlight: f32,
+        blight: f32,
+    ) -> Result<Box<[u8]>, JsValue> {
+        if !is_param(wlight) || !is_param(blight) {
+            return Err("invalid parameter".into());
+        }
+
+        let output = self
+            .inner
+            .grey_output(width, height, checkerboarded, wlight, blight);
+        Ok(output.into_raw().into_boxed_slice())
+    }
+
+    pub fn colorful_output(
+        &self,
+        width: u32,
+        height: u32,
+        checkerboarded: bool,
+        wlight: f32,
+        blight: f32,
+        wcolor: f32,
+        bcolor: f32,
+    ) -> Result<Box<[u8]>, JsValue> {
+        if ![wlight, blight, wcolor, bcolor]
+            .into_iter()
+            .all(|&p| is_param(p))
+        {
+            return Err("invalid parameter".into());
+        }
+        let output = self.inner.colorful_output(
+            width,
+            height,
+            checkerboarded,
+            wlight,
+            blight,
+            wcolor,
+            bcolor,
+        );
+        Ok(output.into_raw().into_boxed_slice())
+    }
 }
